e0626b1010e70f17a9cf5d1fb7dcbaa819cea104
Ken Kieu <kennyykieu@gmail.com>
2021-12-14 02:25:32 +0000

    Fixed typewriter

--- src/pages/about-me.jsx
+++ src/pages/about-me.jsx
@@ -5,25 +5,20 @@ export default function AboutMe() {
   return (
     <div className="max-w-screen-xl mx-auto px-3 md:px-6 pt-12">
       <h1 className="border-solid border-l-4 border-green-800 border-opacity-50 text-3xl px-2 my-12">
-        I&apos;m Ken <br></br>
-        <span className="italic">
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter
-                .typeString("a traveler")
-                .pauseFor(1500)
-                .deleteAll()
-                .typeString("a tech enthusiast")
-                .pauseFor(1500)
-                .deleteAll()
-                .typeString("a gamer")
-                .pauseFor(1500)
-                .deleteAll()
-                .typeString("a web developer")
-                .start();
-            }}
-          />
-        </span>
+        Hello I&apos;m Ken,
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("traveler")
+              .pauseFor(1500)
+              .deleteAll()
+              .typeString("tech enthusiast")
+              .pauseFor(1500)
+              .deleteAll()
+              .typeString("web developer")
+              .start();
+          }}
+        />
       </h1>
       <img
         className="w-full rounded-lg"
