5fe04e4cdd8b80cd4b1518f6afaf0b1d7f63713c
Ken Kieu <kennyykieu@gmail.com>
2021-12-15 02:35:21 +0000

    Edited about me text

--- src/pages/about-me.jsx
+++ src/pages/about-me.jsx
@@ -34,7 +34,8 @@ export default function AboutMe() {
         <p className="leading-8 my-4">
           My name is Ken, but you can think of me as a traveler, tech
           enthusiast, and web developer! I attended college at California State
-          University, Long Beach and received a Bachelor's degree in Psychology.
+          University, Long Beach, and received a Bachelor's degree in
+          Psychology.
           <br />
           <br />
           <em>
@@ -43,40 +44,32 @@ export default function AboutMe() {
           </em>
           <br />
           <br />
-          During my time in the psychology field, I learned invaluable skills,
-          such as patience, communication, and creativity, but I came to the
-          realization that the field was not for me. So, I went back to my
-          foundational interest in technology and started considering my
-          options. I spoke to a friend that found success with web development
-          through a bootcamp named LearningFuze, and he highly recommmended I
-          attend if I was interested.
+          Well yes, while I learned invaluable skills, such as patience,
+          communication, and creativity, I realized that psychology was
+          ultimately not for me. Therefore, I went back to my roots in
+          technology and started considering my options. In my time networking,
+          I met with a friend that found success with web development through a
+          boot camp named LearningFuze, and he highly recommended I attend if I
+          was interested.
         </p>
         <p className="leading-8 my-4">
-          For 9 months, I juggled a 40 hour workweek while simultaneously
-          committing an additional 15 hours per week to learning the basics of
-          HTML5, CSS3, and JavaScript. When the time came, I parted ways with my
-          current job, and began attending LearningFuze where I would dedicate
-          800+ hours on coding during the 14 week program. By the end of the
-          curriculum I had learned how to code a full stack application with
-          PostgreSQL, Express.js, React.js, and Node.js.
-          <br />
-          <br />
-          Since my enrollment to LearningFuze, I have discovered how coding is a
-          diverse creative outlet that allows me to bring my ideas to life, and
-          it has become one of my favorite hobbies. I often find myself thinking
-          about new projects because they provide me with the opportunity to
-          learn and work with new technologies.
+          For months I continued to work my full-time job while learning how to
+          code on my own, and when the time came, I parted ways and enrolled in
+          LearningFuze where I would dedicate 60-70 hours per week to
+          programming. By the end of the curriculum, I had learned how to
+          develop a full-stack application with PostgreSQL, Express.js,
+          React.js, and Node.js.
         </p>
         <p className="leading-8 my-4">
           My drive for learning and self-development has led me to a field that
-          is not only rewarding, but also aligns with my core interests. I am
-          truly thankful to have this opportunity &mdash; but this is only the
+          is not only rewarding but also aligns with my core interests. I am
+          truly thankful to have this opportunity &mdash, but this is only the
           beginning of my journey. I can&apos;t wait to see what the future has
           in store for me!
         </p>
         <p className="leading-8 my-4">
           <span className="font-semibold">Fun Fact:</span> I own 9 pairs of
-          headphones, and my best friend is a Yorkie. &#128054;
+          headphones, and my best pal is a Yorkie. &#128054;
         </p>
       </div>
     </div>
