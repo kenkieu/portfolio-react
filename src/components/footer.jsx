09803f8aa186ced6ae8f36da9817fca41de05123
Ken Kieu <kennyykieu@gmail.com>
2022-02-22 13:07:36 -0800

    Changed email button's email to

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ export default function Footer(props) {
     <footer id="contact" className="bg-gray-100 mt-16 py-2 md:py-6 lg:py-8">
       <div className="flex m-auto w-2/3 justify-center my-8">
         <a
-          href="mailto: kenkieu93@gmail.com"
+          href="mailto: kenkieu.dev@gmail.com"
           className="px-6 text-red-600 md:transform hover:-translate-y-0.5 duration-500"
         >
           <i className="fas fa-envelope-square text-4xl"></i>
