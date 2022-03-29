1d72534b0a1bbc2718f39f86249b4297d5171065
Ken Kieu <kennyykieu@gmail.com>
2022-03-29 08:57:03 -0700

    Mobile responsiveness for contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -46,8 +46,8 @@ const onSubmit = async (event) => {
           Contact Me
         </h3>
       </div>
-      <div className="grid grid-cols-4 gap-12">
-        <div className="col-span-2 px-12">
+      <div className="sm:grid grid-cols-4 gap-12">
+        <div className="mx-auto col-span-2 px-8 mb-8 sm:mb-6">
           <h2 className="text-xl leading-8 font-bold tracking-tight text-gray-900 md:text-2xl">
             Get In Touch
           </h2>
@@ -64,19 +64,20 @@ const onSubmit = async (event) => {
           }
           <form onSubmit={onSubmit} className="mx-auto">
             <label className="sr-only" htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" placeholder="Name" required className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 border-gray-300 rounded-md mb-6" onChange={createMessage}/>
+            <input type="text" id="name" name="name" placeholder="Name" required className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 border-gray-300 rounded-md mb-8 sm:mb-6" onChange={createMessage}/>
             <label className="sr-only" htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Email" required className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 border-gray-300 rounded-md mb-6" onChange={createMessage} />
+            <input type="email" id="email" name="email" placeholder="Email" required className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 border-gray-300 rounded-md mb-8 sm:mb-6" onChange={createMessage} />
             <label className="sr-only" htmlFor="message">Message</label>
             <textarea
               id="message"
               name="message"
               placeholder="Message"
               required
-              className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 border-gray-300 rounded-md mb-6"
+              className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 border-gray-300 rounded-md mb-8 sm:mb-6"
               onChange={createMessage}
+              rows="6"
             ></textarea>
-            <button type="submit" disabled={submitting} className="inline-flex justify-center py-2 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-green-800 bg-opacity-70 hover:bg-opacity-80 focus:outline-none">Send</button>
+            <button type="submit" disabled={submitting} className="inline-flex justify-center py-2 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-green-800 bg-opacity-70 hover:bg-opacity-80 w-full sm:w-auto">Send</button>
           </form>
         </div>
       </div>
