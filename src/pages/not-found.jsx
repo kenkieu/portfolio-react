a9efec188ad4fa662cce7183172ee7b284c2cb48
Ken Kieu <kennyykieu@gmail.com>
2022-03-30 07:01:58 -0700

    Update margin for not found page

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -2,8 +2,11 @@ import React, {useEffect} from 'react';
 import { Link } from "react-router-dom";
 
 export default function NotFound() {
+  useEffect(() => {
+    document.title = "404 | Not Found"
+  }, []);
   return (
-    <div className="min-h-screen mx-auto px-4 md:px-6 mt-24">
+    <div className="min-h-screen mx-auto px-4 md:px-6">
       <div className="max-w-screen-xl mx-auto banner">
         <div className="flex items-center lg:justify-center flex-wrap lg:flex-wrap">
           <span className="text-center text-4xl mr-6 leading-8 font-extrabold tracking-tight text-gray-900 sm:text-5xl">
@@ -17,7 +20,7 @@ export default function NotFound() {
               <p className="text-gray-400 py-2">
                 Please verify the address in the URL and try again.
               </p>
-              <span className="text-green-800 hover:text-opacity-90 text-opacity-70 mr-6 mt-12">
+              <span className="text-green-800 hover:text-opacity-90 text-opacity-70 mr-6 mt-12 font-extrabold">
                 <Link to="/">Return Home</Link>
                 <span aria-hidden="true"> →</span>
               </span>
