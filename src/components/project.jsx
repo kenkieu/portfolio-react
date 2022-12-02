7c05c71373df82530be086877cce691f823c88c5
Ken Kieu <kennyykieu@gmail.com>
2022-12-01 18:26:34 -0800

    Enhance readability, destructuring props

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,23 +1,26 @@
 export default function Project(props) {
+
+  const {website, image, title, description, technologies} = props
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-      <a href={props.website}>
+      <a href={website}>
         <img
           className="w-full rounded-lg shadow-md lg:transform hover:-translate-x-1 duration-700"
-          src={props.image}
+          src={image}
           alt="website"
         />
       </a>
       <div className="flex flex-wrap content-center text-gray-500">
-        <a href={props.website}>
+        <a href={website}>
           <h3 className="text-center text-lg text-gray-700 hover:text-black font-semibold duration-700">
-            {props.title} <i className="fas fa-external-link-alt text-xs"></i>
+            {title} <i className="fas fa-external-link-alt text-xs"></i>
           </h3>
         </a>
-        <p className="leading-8 my-4">{props.description}</p>
+        <p className="leading-8 my-4">{description}</p>
         <p className="text-sm italic">
           <span className="font-semibold text-gray-700">Technologies: </span>
-          {props.technologies}
+          {technologies}
         </p>
       </div>
     </div>
