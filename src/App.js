2c6e2119f8da3f5930a621f5815a5c509296d850
Ken Kieu <kennyykieu@gmail.com>
2022-12-01 19:55:33 -0800

    Add gtm/ga4 tracking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import TagManager from "react-gtm-module";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import AboutMe from "./pages/about-me";
@@ -8,6 +9,14 @@ import Contact from "./pages/contact";
 import NotFound from "./pages/not-found";
 
 export function App() {
+
+  //Config for google tag manager
+  const tagManagerArgs = {
+    gtmId = 'GTM-5SR4CDB',
+  }
+
+  TagManager.initialize(tagManagerArgs)
+
   function scrollTop() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }
